705c36046b319fd845f5dc5c40f87e88ed163834
Hillo111 <stas.gannutin@gmail.com>
2024-01-13 10:15:58 -0600

    move auto to valk folder

--- src/main/include/valkyrie/Auto.h
+++ src/main/include/valkyrie/Auto.h
@@ -3,17 +3,20 @@
 #include <frc/TimedRobot.h>
 #include <Drivetrain.h>
 
+namespace valor {
+
 class Auto
 {
     public:
-        Auto(Drivetrain *drivetrain);
+        Auto();
         
         void fillAutoList();
         frc2::CommandPtr getCurrentAuto();
 
     private:
-        Drivetrain *drivetrain;
         frc2::CommandPtr makeAuto(std::string autoName);
         std::shared_ptr<nt::NetworkTable> table;
         frc::SendableChooser<std::string> m_chooser;
 };
+
+}
