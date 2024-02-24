eecb55c2830c45f4ef6587cefc55d01822f14c1e
ch1kim0n1 <chikimoni61@gmail.com>
2024-02-24 10:19:23 -0600

    error fix

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -20,6 +20,7 @@
 #include <subsystems/Shooter.h>
 #include "subsystems/Feeder.h"
 #include "subsystems/Climber.h"
+#include "subsystems/Leds.h"
 
 
 #include <fstream>
@@ -41,7 +42,6 @@ class Robot : public frc::TimedRobot {
         void AutonomousExit() override;
         
     private:
-        std::shared_ptr<nt::NetworkTable> nt_robot = nt::NetworkTableInstance::GetDefault().GetTable("Robot");
         valor::Gamepad gamepadOperator{OIConstants::GAMEPAD_OPERATOR_LOCATION};
         valor::Gamepad gamepadDriver{OIConstants::GAMEPAD_BASE_LOCATION};
 
