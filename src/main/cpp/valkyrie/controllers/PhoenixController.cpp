c87357b26245ddd2278d86f27af35f1fe998b868
jhelgeson <jan@helgeson.co>
2024-03-03 17:18:36 -0600

    added enable FOC overload control method for setPostion

--- src/main/cpp/valkyrie/controllers/PhoenixController.cpp
+++ src/main/cpp/valkyrie/controllers/PhoenixController.cpp
@@ -267,7 +267,15 @@ void PhoenixController::setRange(int slot, double min, double max)
 void PhoenixController::setPosition(double position)
 {
     req_position.Position = units::make_unit<units::turn_t>(position); // Mechanism rotations
-    req_position.EnableFOC = true;
+    status = motor->SetControl(req_position);
+}
+/**
+ * Set a position in mechanism rotations, use FOC
+*/
+void PhoenixController::setPosition(double position, bool enableFOC)
+{
+    req_position.Position = units::make_unit<units::turn_t>(position); // Mechanism rotations
+    req_position.EnableFOC = enableFOC;
     status = motor->SetControl(req_position);
 
 }
